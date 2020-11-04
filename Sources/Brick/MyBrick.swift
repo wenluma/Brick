9d3e05768c7d90f867b08698560dff0286d602f6
wenluma <wenluma_521@163.com>
2020-11-04 08:03:30 +0800

    剑指offer-重复数字
    通过交互下标来实现

--- Sources/Brick/MyBrick.swift
+++ Sources/Brick/MyBrick.swift
@@ -13,9 +13,15 @@ import Foundation
  [1,2,3,4,1], 重复数字 1
  [2,4,6,1,2], 重复 2
  解题思路，将利用数字的边界，将数组元素，转换成 index，在进行计算， 可以在 o(1) 的时间操作
+ 
+ 实现方式：
+ 1. 排序
+ 2. map/array下标 对应
+ 3. 不停的交互下标，来互换
+ 4. 不交互下标，分段统计个数
  */
 public class DuplicateNumbers  {
-  // 如果返回 true 代表 找到重复出现的数数字，false 否则没有
+  // 如果返回 true 代表 找到重复出现的数数字，false 否则没有， array 索引
   public func isDuplicate(source: [Int], target: inout Int?) -> Bool {
     // 边界，提前返回
     if source.isEmpty || source.count == 1 {
@@ -38,6 +44,60 @@ public class DuplicateNumbers  {
         return true
       } else {
         indexs[item] = value + 1
+      }
+    }
+    return false
+  }
+  // 改变数组，交换
+  func isDuplicate2(_  source: inout [Int], target: inout Int?) -> Bool {
+    if source.isEmpty || source.count == 1 {
+      return false
+    }
+    
+    for idx in source.indices {
+//      s[a] , s[b], 如果s[a] 与 s[b]
+//     s[0] = 1  s[1] = 2, s[2] = 3, s[3] = 2
+      let current = source[idx]
+      while source[idx] != idx {
+        if source[idx] == source[source[idx]] {
+          target = source[idx]
+          return true
+        }
+//        source.swapAt(idx, source[idx])
+        let tmp = source[idx]
+        source[idx] = source[tmp]
+        source[tmp] = tmp
+      }
+    }
+    return false
+  }
+}
+
+public class DuplicateNumbers2  {
+  // 改变数组，交换
+  func isDuplicate(source: inout [Int], target: inout Int?) -> Bool {
+    if source.isEmpty || source.count == 1 {
+      return false
+    }
+    
+    for item in source {
+      if item < 0 || item > source.count - 1 {
+        return false
+      }
+    }
+    
+    for idx in source.indices {
+//      s[a] , s[b], 如果s[a] 与 s[b]
+//     s[0] = 1  s[1] = 2, s[2] = 3, s[3] = 2
+      while source[idx] != idx {
+        if source[idx] == source[source[idx]] {
+          target = source[idx]
+          return true
+        }
+        source.swapAt(idx, source[idx])
+//        let tmp = source[idx]
+//        source[idx] = source[tmp]
+//        source[tmp] = tmp
       }
     }
     return false
--- Tests/BrickTests/MyBrickTests.swift
+++ Tests/BrickTests/MyBrickTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import Brick
 
 final class MyBrickTests: XCTestCase {
+  // 改变索引
   func testDupicateNumbers() {
     let item = DuplicateNumbers()
 
@@ -26,6 +27,36 @@ final class MyBrickTests: XCTestCase {
       let s0 = [0,4]
       var target: Int? = nil
       let result = item.isDuplicate(source: s0, target: &target)
+      XCTAssertFalse(result)
+      XCTAssertEqual(target, nil)
+    } while false
+  }
+  
+  // 交互位置
+  func testDupicateNumbers2() {
+    let item = DuplicateNumbers2()
+
+    repeat {
+      var s0 = [1,3,4,5,8,0,1,9,6,0]
+      var target: Int? = nil
+      let result = item.isDuplicate(source: &s0, target: &target)
+      XCTAssertTrue(result)
+      XCTAssertEqual(target, 1)
+    } while false
+    
+    repeat {
+      var s0 = [0]
+      var target: Int? = nil
+      let result = item.isDuplicate(source: &s0, target: &target)
+      XCTAssertFalse(result)
+      XCTAssertEqual(target, nil)
+    } while false
+    
+    repeat {
+      // 内容边界
+      var s0 = [0,4]
+      var target: Int? = nil
+      let result = item.isDuplicate(source: &s0, target: &target)
       XCTAssertFalse(result)
       XCTAssertEqual(target, nil)
     } while false
